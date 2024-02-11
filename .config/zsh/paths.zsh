58d971c64b0d827150d70aa2c1f1ffdfcfa7de97
RyugaXhypeR <ryugashyper@gmail.com>
2024-02-11 13:27:32 +0530

    feat: added `bob` to path

--- .config/zsh/paths.zsh
+++ .config/zsh/paths.zsh
@@ -1,8 +1,8 @@
 export PATH=$PATH:"$HOME/.cargo/env"
 export PATH=$PATH:"$HOME/.local/bin"
 export PATH=$PATH:"$HOME/.spicetify"
+export PATH=$PATH:"$HOME/.local/share/bob/nvim-bin"
 export PATH=$PATH:"/opt/gradle/gradle-8.4/bin"
 
 export PYENV_ROOT="$HOME/.pyenv"
 command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin":$PATH
-
