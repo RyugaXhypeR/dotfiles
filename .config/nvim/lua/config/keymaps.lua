21c66441d73e11e2c68f1eba7711a54e5516ed55
RyugaXhypeR <ryugashyper@gmail.com>
2024-05-08 00:20:49 +0530

    feat: setup keybinds for `molten.nvim` and `quarto.nvim`

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,10 +1,19 @@
-if vim.lsp.inlay_hint then
-	vim.keymap.set("n", "<leader>ch", function()
-		vim.lsp.inlay_hint.enable(0, not vim.lsp.inlay_hint.is_enabled())
-	end, { desc = "Toggle Inlay Hints" })
-end
+-- Molten
+vim.keymap.set("n", "<leader>mi", ":MoltenInit<CR>", { desc = "Molten init", silent = true })
+vim.keymap.set("n", "<leader>e", ":MoltenEvaluateOperator<CR>", { desc = "evaluate operator", silent = true })
+vim.keymap.set("n", "<leader>os", ":noautocmd MoltenEnterOutput<CR>", { desc = "open output window", silent = true })
+vim.keymap.set("n", "<leader>rr", ":MoltenReevaluateCell<CR>", { desc = "re-eval cell", silent = true })
+vim.keymap.set("v", "<leader>mr", ":<C-u>MoltenEvaluateVisual<CR>gv", { desc = "execute visual selection", silent = true })
+vim.keymap.set("n", "<leader>oh", ":MoltenHideOutput<CR>", { desc = "close output window", silent = true })
+vim.keymap.set("n", "<leader>md", ":MoltenDelete<CR>", { desc = "delete Molten cell", silent = true })
 
--- Toggle term
--- vim.keymap.set("n", "<C-i>", ":ToggleTerm direction=float<cr>", { desc = "Toggle Float Term" })
--- vim.keymap.set("n", "<C-h>", ":ToggleTerm direction=horizontal<cr>", { desc = "Toggle Horizontal Term" })
--- vim.keymap.set("n", "<C-v>", ":ToggleTerm direction=vertical<cr>", { desc = "Toggle Vertical Term" })
+-- Quarto
+local runner = require("quarto.runner")
+vim.keymap.set("n", "<leader>rc", runner.run_cell, { desc = "run cell", silent = true })
+vim.keymap.set("n", "<leader>ra", runner.run_above, { desc = "run cell and above", silent = true })
+vim.keymap.set("n", "<leader>rA", runner.run_all, { desc = "run all cells", silent = true })
+vim.keymap.set("n", "<leader>rl", runner.run_line, { desc = "run line", silent = true })
+vim.keymap.set("v", "<leader>r", runner.run_range, { desc = "run visual range", silent = true })
+vim.keymap.set("n", "<leader>RA", function()
+	runner.run_all(true)
+end, { desc = "run all cells of all languages", silent = true })
