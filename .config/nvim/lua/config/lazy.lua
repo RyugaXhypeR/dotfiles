2484834b19588be642981b8bdae21872e7f6b3d6
RyugaXhypeR <ryugashyper@gmail.com>
2024-05-08 00:17:05 +0530

    feat: added plugins to `disabled_plugins` list

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -19,11 +19,13 @@ require("lazy").setup({
 	checker = { enabled = true },
 	performance = {
 		rtp = {
-			-- disable some rtp plugins
 			disabled_plugins = {
 				"gzip",
+				"matchit",
+				"matchparen",
 				"netrwPlugin",
 				"tarPlugin",
+				"tohtml",
 				"tutor",
 				"zipPlugin",
 			},
