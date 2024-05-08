c6b57a21c7dc3fa82829a9dd8250aa1450eb7502
RyugaXhypeR <ryugashyper@gmail.com>
2024-05-08 18:50:59 +0530

    feat: updated `lsp` config

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -40,16 +40,31 @@ return {
 	},
 	{
 		"neovim/nvim-lspconfig",
+		opts = {
+			diagnostics = {
+				underline = false,
+				update_in_insert = true,
+				virtual_text = {
+					enabled = true,
+					prefix = "",
+					spacing = 4,
+				},
+			},
+			inlay_hints = {
+				enabled = true,
+			},
+			codelens = {
+				enabled = true,
+			},
+		},
+
 		servers = {
-			asm_lsp = {},
-			bashls = {},
-			clangd = {},
-			cmake = {},
-			cssls = {},
-			htmlls = {},
-			dockerls = {},
-			texlab = {},
-			jsonls = {},
+			asm_lsp = {
+				command = "asm-lsp",
+				filetypes = { "asm", "s", "S" },
+				bashls = {},
+			},
+			clangd = { "c", "cpp", "c++", "cxx", "cc", "h" },
 
 			lua_ls = {
 				single_file_support = true,
@@ -116,25 +131,40 @@ return {
 					},
 				},
 			},
-		},
+			pyright = {
+				capabilities = function(_, _)
+					local capabilities = require("lsp-status").capabilities
+					capabilities.workspace.didChangeConfiguration.dynamicRegistration = false
+					return capabilities
+				end,
+				settings = {
+					python = {
+						analysis = {
+							diagnosticSeverityOverrides = {
+								reportWildcardImportFromLibrary = "none",
+								reportUnusedImport = "information",
+								reportUnusedClass = "information",
+								reportUnusedFunction = "information",
+							},
+						},
+					},
+					pyright = {
+						disableTaggedHints = true,
+					},
+				},
+			},
 
-		pyright = {
-			settings = {
-				python = {
-					analysis = {
-						autoSearchPaths = true,
-						useLibraryCodeForTypes = true,
-						diagnosticMode = "workspace",
-						typeCheckingMode = "basic",
-						stubPath = vim.fn.stdpath("data") .. "/stubs",
-						stubs = {
-							"numpy-stubs",
-							"pandas-stubs",
-							"matplotlib-stubs",
-							"scipy-stubs",
-							"sklearn-stubs",
-							"torch-stubs",
-							"torchvision-stubs",
+			rust = {
+				settings = {
+					["rust-analyzer"] = {
+						checkOnSave = {
+							command = "clippy",
+						},
+						cargo = {
+							loadOutDirsFromCheck = true,
+						},
+						procMacro = {
+							enable = true,
 						},
 					},
 				},
@@ -143,11 +173,49 @@ return {
 	},
 
 	{
-		"lervag/vimtex",
-		init = function()
-			vim.g.vimtex_compiler_progname = "nvr"
-			vim.g.vimtex_view_method = "zathura"
+		"mrcjkb/rustaceanvim",
+		version = "^4", -- Recommended
+		ft = { "rust" },
+		opts = {
+			server = {
+				on_attach = function(_, bufnr)
+					vim.keymap.set("n", "<leader>cR", function()
+						vim.cmd.RustLsp("codeAction")
+					end, { desc = "Code Action", buffer = bufnr })
+					vim.keymap.set("n", "<leader>dr", function()
+						vim.cmd.RustLsp("debuggables")
+					end, { desc = "Rust Debuggables", buffer = bufnr })
+				end,
+				default_settings = {
+					-- rust-analyzer language server configuration
+					["rust-analyzer"] = {
+						cargo = {
+							allFeatures = true,
+							loadOutDirsFromCheck = true,
+							buildScripts = {
+								enable = true,
+							},
+						},
+						-- Add clippy lints for Rust.
+						checkOnSave = {
+							allFeatures = true,
+							command = "clippy",
+							extraArgs = { "--no-deps" },
+						},
+						procMacro = {
+							enable = true,
+							ignored = {
+								["async-trait"] = { "async_trait" },
+								["napi-derive"] = { "napi" },
+								["async-recursion"] = { "async_recursion" },
+							},
+						},
+					},
+				},
+			},
+		},
+		config = function(_, opts)
+			vim.g.rustaceanvim = vim.tbl_deep_extend("keep", vim.g.rustaceanvim or {}, opts or {})
 		end,
 	},
-	{ "elkowar/yuck.vim" },
 }
